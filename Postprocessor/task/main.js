3370d0cb9c843f7fb8d4bea6b53a8434e76acfbf
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-07-09 10:08:35 +0200

    Stage 1/3

--- Postprocessor/task/main.js
+++ Postprocessor/task/main.js
@@ -1,1 +1,16 @@
-// write your code here
+fs = require('fs');
+
+
+function processDatabase(filePath) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const lines = data.split('\n');
+        const users = lines.filter(line => line.split(', ')[1] !== '-');
+
+        const lastUser = users[users.length - 1].split(', ');
+        console.log(`The user ${lastUser[1]} has "${lastUser[3]}" consent status for sending emails`);
+    });
+}
+
+processDatabase('database.csv');
